508f490b6a9e8aef03a4cce1d6950429fbad4c29
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-22 09:25:06 +0300

    feat: text/plain response

--- src/main/java/app/poja/exam/endpoint/rest/controller/BigSumController.java
+++ src/main/java/app/poja/exam/endpoint/rest/controller/BigSumController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.math.BigInteger;
 
 @RestController
@@ -11,8 +12,10 @@ public class BigSumController {
   @GetMapping("/big-sum")
   public BigInteger getBigSum(
       @RequestParam(defaultValue="0", required=false) BigInteger a,
-      @RequestParam(defaultValue="0", required=false) BigInteger b
+      @RequestParam(defaultValue="0", required=false) BigInteger b,
+      HttpServletResponse response
   ){
+    response.addHeader("content-type", "text/plain");
     return a.add(b);
   }
 }
