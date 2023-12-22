5fca34163792290a783599e22e5e4250b0b75396
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-22 09:41:52 +0300

    feat: string representation

--- src/main/java/app/poja/exam/endpoint/rest/controller/BigSumController.java
+++ src/main/java/app/poja/exam/endpoint/rest/controller/BigSumController.java
@@ -10,12 +10,12 @@ import java.math.BigInteger;
 @RestController
 public class BigSumController {
   @GetMapping("/big-sum")
-  public BigInteger getBigSum(
+  public String getBigSum(
       @RequestParam(defaultValue="0", required=false) BigInteger a,
       @RequestParam(defaultValue="0", required=false) BigInteger b,
       HttpServletResponse response
   ){
     response.addHeader("content-type", "text/plain");
-    return a.add(b);
+    return a.add(b).toString();
   }
 }
